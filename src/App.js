5aa19743aa7b7d591c411f5820abc099353b8a05
NicolasCessieux <nicolascessieux@hotmail.fr>
2019-04-13 19:49:32 +0200

    commit du dossier src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Home from './Home';
+import History from './History';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <NavLink
+            to="/"
+            style={{padding: "12px"}}
+            activeStyle={{fontWeight: "bold", color: "red", padding: "12px"}}
+          >Home
+          </NavLink>
+          <NavLink
+            to="/notre-histoire"
+            style={{padding: "12px"}}
+            activeStyle={{fontWeight: "bold", color: "red", padding: "12px"}}
+          >History
+          </NavLink>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/notre-histoire" component={History}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+    <div >
+        <h1>Bienvenue sur l'accueil</h1>
+    </div>
+  );
+
+export default Home;
